1453126b02e2d6c31e4353d4a6e399e86b97f854
ldiazcto <ldiazc@fi.uba.ar>
2022-11-11 17:55:00 -0300

    topo

--- topologia.py
+++ topologia.py
@@ -1,20 +1,23 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
 
-class Topo():
+class MyTopo(Topo):
     def __init__( self ):
         # Initialize topology
         Topo.__init__( self )
         
         # Create switch
-        s1 = self.addSwitch('switch_1')
-        s2 = self.addSwitch('switch_2')
+        s1 = self.addSwitch("switch_1")
+        #s2 = self.addSwitch('switch_2')
         # Create hosts
-        h1 = self.addHost('host_1 ')
-        h2 = self.addHost('host_2')
+        h1 = self.addHost("host_1")
+        h2 = self.addHost("host_2")
         
         # Add links between switches and hosts self . addLink (s1 , s2)
         self.addLink(s1 , h1)
         self.addLink(s1 , h2)
 
-topos = { 'customTopo ': Topo }
+#topos = { 'customTopo ': Topo }
+topos = {'mytopo': MyTopo}
+#
+#sudo mn --custom ./topologia.py --topo mytopo --mac --switch ovsk
